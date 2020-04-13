9b7d253b0f3631c1c6c47b74df8e125b182e7e06
Sanka Sanjeewa <tharuka@dspconstrols.lk>
2020-04-13 20:08:35 +0530

    About, profile components are generated. changed some css variable to css properties. Dark theme was added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { HomeComponent } from './components/home/home.component';
-import { LoginComponent } from './components/login/login.component';
+import { AboutComponent } from './components/about/about.component';
 import { RegisterComponent } from './components/register/register.component';
+import {ProfileComponent } from './components/profile/profile.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
   {path: '', redirectTo: '/home', pathMatch: 'full'},
   {path: 'home', component: HomeComponent},
+  {path: 'about', component: AboutComponent},
+  {path: 'register', component: RegisterComponent},
+  {path: 'profile', component: ProfileComponent},
   {path: 'login', component: LoginComponent},
-  {path: 'register', component: RegisterComponent}
   // {path: '**', component: notFoundComponent}
 ];
 
@@ -17,4 +21,4 @@ const routes: Routes = [
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
-export const routingComponents = [HomeComponent, LoginComponent, RegisterComponent]
+export const routingComponents = [HomeComponent, AboutComponent, RegisterComponent, ProfileComponent, LoginComponent];
--- src/app/components/side-nav/side-nav.component.ts
+++ src/app/components/side-nav/side-nav.component.ts
@@ -7,9 +7,33 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SideNavComponent implements OnInit {
 
-  constructor() { }
+  isDarkMode = false;
+
+  themeMap = {
+    light: 'dark',
+    dark: 'light'
+  };
+
+  tmp = Object.keys(this.themeMap)[0];
+  theme = localStorage.getItem('theme') || (this.tmp, localStorage.setItem('theme', this.tmp), this.tmp);
+
+  constructor() {
+    document.body.classList.add(this.theme);
+  }
 
   ngOnInit(): void {
   }
 
+  toggleTheme() {
+    const current = localStorage.getItem('theme');
+    const next = this.themeMap[current];
+    document.body.classList.replace(current, next);
+    localStorage.setItem('theme', next);
+
+    if (current === 'dark') {
+      this.isDarkMode = true;
+    } else {
+      this.isDarkMode = false;
+    }
+  }
 }
