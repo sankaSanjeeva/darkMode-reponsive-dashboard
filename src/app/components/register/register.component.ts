806ca3998d7eb70707eea915ac484a726e074c17
Sanka Sanjeewa <tharuka@dspconstrols.lk>
2020-06-05 07:39:57 +0530

    created the registration page

--- src/app/components/register/register.component.ts
+++ src/app/components/register/register.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, FormArray, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-register',
@@ -7,9 +8,44 @@ import { Component, OnInit } from '@angular/core';
 })
 export class RegisterComponent implements OnInit {
 
-  constructor() { }
+  regForm: FormGroup;
 
-  ngOnInit(): void {
+  constructor(private fb: FormBuilder) { }
+
+  ngOnInit() {
+    this.regForm = this.fb.group({
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]],
+      password: ['', [
+        Validators.required,
+        Validators.pattern('^(?=.*[0-9])(?=.*[a-zA-Z])([a-zA-Z0-9]+)$')
+      ]],
+      age: [null, [
+        Validators.required,
+        Validators.minLength(2),
+        Validators.min(18),
+        Validators.max(65)
+      ]]
+    });
   }
 
+  get email() {
+    return this.regForm.get('email');
+  }
+
+  get password() {
+    return this.regForm.get('password');
+  }
+
+  get age() {
+    return this.regForm.get('age');
+  }
+
+  async submitForm() {
+    console.log(this.regForm.value);
+    console.warn(this.regForm.value);
+    
+  }
 }
